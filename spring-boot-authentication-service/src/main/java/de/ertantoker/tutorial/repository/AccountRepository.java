8e902397ffc8ca006de5aa54bea78e8cff1b850f
Karol Ślusarz <pr_karols@altar.com.pl>
2019-07-12 11:56:12 +0200

    Convert to JPA repository

--- spring-boot-authentication-service/src/main/java/de/ertantoker/tutorial/repository/AccountRepository.java
+++ spring-boot-authentication-service/src/main/java/de/ertantoker/tutorial/repository/AccountRepository.java
@@ -1,11 +1,13 @@
 package de.ertantoker.tutorial.repository;
 
 import de.ertantoker.tutorial.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface AccountRepository extends MongoRepository<Account, String> {
-
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    @Query("SELECT u FROM Account u WHERE u.username=?1")
     Optional<Account> findOneByUsername(String username);
 }
